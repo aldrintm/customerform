eb6665faccffd831fa3ee0e85ed1822fd2dbd1c3
Aldrin <aldrin@plamarusa.com>
2024-11-18 16:51:27 -0800

    right side form updated

--- components/CustomerForm.jsx
+++ components/CustomerForm.jsx
@@ -33,9 +33,10 @@ const NewCustomerForm = () => {
           <form className='container mx-auto my-2 justify-center'>
             <div className='grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-4 pb-2'>
               {/* Left Side of Form Column */}
+
               <div className='grid grid-cols-1 gap-4 lg:row-auto lg:gap-4 bg-white py-6 border rounded-md shadow-md'>
                 {/* First Name and Last Name */}
-                <div className='grid grid-cols-1 gap-4 lg:grid-cols-2 px-4 lg:gap-x-8'>
+                <div className='grid grid-cols-1 gap-4 lg:grid-cols-2 px-4 lg:gap-x-6'>
                   {/* First Name */}
                   <div>
                     <label
@@ -76,7 +77,7 @@ const NewCustomerForm = () => {
                 </div>
 
                 {/* Phone and Email */}
-                <div className='grid grid-cols-1 gap-4 lg:grid-cols-2 px-4 lg:gap-x-8'>
+                <div className='grid grid-cols-1 gap-4 lg:grid-cols-2 px-4 lg:gap-x-6'>
                   {/* Phone */}
                   <div>
                     <label
@@ -87,7 +88,7 @@ const NewCustomerForm = () => {
                     </label>
 
                     <input
-                      type='text'
+                      type='tel'
                       id='phone'
                       name='phone'
                       required
@@ -117,7 +118,7 @@ const NewCustomerForm = () => {
                 </div>
 
                 {/* Address */}
-                <div className='grid grid-cols-1 gap-4 px-4 lg:gap-8'>
+                <div className='grid grid-cols-1 gap-4 px-4 lg:gap-x-6'>
                   {/* Street Address */}
                   <div>
                     <label
@@ -139,7 +140,7 @@ const NewCustomerForm = () => {
                 </div>
 
                 {/* City + State + Zipcode */}
-                <div className='grid grid-cols-1 gap-4 md:gap-x-2 md:grid-cols-8 px-4 lg:gap-x-4 xl:gap-x-8'>
+                <div className='grid grid-cols-1 gap-4 md:gap-x-4 md:grid-cols-8 px-4 lg:gap-x-6'>
                   {/* City */}
                   <div className='md:col-span-5'>
                     <label
@@ -199,7 +200,7 @@ const NewCustomerForm = () => {
                 </div>
 
                 {/* Contractor Info */}
-                <div className='grid grid-cols-1 md:grid-cols-2 gap-4 px-4 lg:gap-8'>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-4 px-4 lg:gap-x-6'>
                   {/* Contractor Name */}
                   <div>
                     <label
@@ -243,11 +244,11 @@ const NewCustomerForm = () => {
               {/* Break */}
 
               {/* Right Side of the Form Column */}
-              <div className='grid grid-cols-1 gap-4 lg:row-auto lg:gap-4 bg-white py-6 border rounded-md shadow-md'>
+              <div className='grid grid-cols-1 gap-4 lg:grid-rows-auto lg:gap-4 bg-white py-6 border rounded-md shadow-md'>
                 {/* Purchase Order + Store ID */}
-                <div className='grid grid-cols-1 gap-4 lg:grid-cols-2 px-4 lg:gap-x-8'>
+                <div className='grid grid-cols-1 gap-4 lg:grid-cols-2 px-4 lg:gap-x-6'>
                   {/* Purchase Order */}
-                  <div>
+                  <div className='p-0'>
                     <label
                       htmlFor='purchaseOrder'
                       className='block text-xs md:text-sm pl-1 font-semibold text-gray-500'
@@ -277,7 +278,7 @@ const NewCustomerForm = () => {
                     <select
                       name='storeId'
                       id='storeId'
-                      className='mt-1 w-full rounded-md shadow-sm sm:text-sm bg-sky-50'
+                      className='mt-1 w-full rounded-md shadow-sm sm:text-sm bg-sky-50 border-gray-200 focus-bg-white'
                       defaultValue={'DEFAULT'}
                     >
                       <option value='DEFAULT' disabled>
@@ -289,6 +290,209 @@ const NewCustomerForm = () => {
                       <option value='Builder'>Builder</option>
                       <option value='KandB'>Kitchen and Bath</option>
                     </select>
+                  </div>
+                </div>
+
+                {/* PO Date and Amount Paid */}
+                <div className='grid grid-cols-1 gap-4 lg:grid-cols-12 px-4 lg:gap-x-6'>
+                  {/* Purchase Order Date */}
+                  <div className='col-span-4'>
+                    <label
+                      htmlFor='purchaseOrderDate'
+                      className='block text-xs md:text-sm pl-1 font-semibold text-gray-500'
+                    >
+                      Date customer paid?
+                    </label>
+
+                    <input
+                      type='date'
+                      id='purchaseOrderDate'
+                      name='purchaseOrderDate'
+                      placeholder='Purchase order date'
+                      className='mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm bg-sky-50'
+                    />
+                  </div>
+
+                  {/* Purchase Order Amount */}
+                  <div className='col-span-4'>
+                    <label
+                      htmlFor='purchaseOrderAmount'
+                      className='block text-xs md:text-sm pl-1 font-semibold text-gray-500'
+                    >
+                      Purchase Order Amount
+                    </label>
+
+                    <input
+                      type='text'
+                      name='purchaseOrderAmount'
+                      id='purchaseOrderAmount'
+                      placeholder='How much did the customer paid?'
+                      className='mt-1 w-full rounded-md shadow-sm sm:text-sm bg-sky-50 border-gray-200 focus-bg-white'
+                    />
+                  </div>
+
+                  {/* Square Feet */}
+                  <div className='col-span-4'>
+                    <label
+                      htmlFor='squareFeet'
+                      className='block text-xs md:text-sm pl-1 font-semibold text-gray-500'
+                    >
+                      Paid Sq Ft.
+                    </label>
+
+                    <input
+                      type='text'
+                      id='squareFeet'
+                      name='squareFeet'
+                      placeholder='How many SqFt?'
+                      className='mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm bg-sky-50'
+                    />
+                  </div>
+                </div>
+
+                {/* Purchase Order Info - Material Brand + Thickness + Color */}
+                <div className='grid grid-cols-1 gap-4 lg:grid-cols-12 px-4 lg:gap-x-6'>
+                  {/* Material Type */}
+                  <div className='col-span-3'>
+                    <label
+                      htmlFor='materialType'
+                      className='block text-xs md:text-sm pl-1 font-semibold text-gray-500'
+                    >
+                      Type
+                    </label>
+
+                    <select
+                      id='materialType'
+                      name='materialType'
+                      defaultValue={'default'}
+                      className='mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm bg-sky-50'
+                    >
+                      <option value='default' disabled>
+                        ---
+                      </option>
+                      <option value='Quartz'>Quartz</option>
+                      <option value='Granite'>Granite</option>
+                      <option value='SolidSurface'>Solid Surface</option>
+                      <option value='Porcelain'>Porcelain</option>
+                      <option value='Marble'>Marble</option>
+                      <option value='Limestone'>Limestone</option>
+                      <option value='Travertine'>Travertine</option>
+                      <option value='PreFab'>PreFab</option>
+                      <option value='Other'>Other</option>
+                    </select>
+                  </div>
+
+                  {/* Material Thickness */}
+                  <div className='col-span-2'>
+                    <label
+                      htmlFor='thickness'
+                      className='block text-xs md:text-sm pl-1 font-semibold text-gray-500'
+                    >
+                      Thickness
+                    </label>
+
+                    <select
+                      name='thickness'
+                      id='thickness'
+                      className='mt-1 w-full rounded-md shadow-sm sm:text-sm bg-sky-50 border-gray-200 focus-bg-white'
+                      defaultValue={'default'}
+                    >
+                      <option value='default' disabled>
+                        ---
+                      </option>
+                      <option value='2cm'>2cm</option>
+                      <option value='3cm'>3cm</option>
+                      <option value='.5inch'>1/2"</option>
+                      <option value='12mm'>12mm</option>
+                      <option value='Other'>Other</option>
+                    </select>
+                  </div>
+
+                  {/* Material Brand */}
+                  <div className='col-span-3'>
+                    <label
+                      htmlFor='materialBrand'
+                      className='block text-xs md:text-sm pl-1 font-semibold text-gray-500'
+                    >
+                      Brand
+                    </label>
+
+                    <select
+                      name='materialBrand'
+                      id='materialBrand'
+                      className='mt-1 w-full rounded-md shadow-sm sm:text-sm bg-sky-50 border-gray-200 focus-bg-white'
+                      defaultValue={'default'}
+                    >
+                      <option value='default' disabled>
+                        ---
+                      </option>
+                      <option value='Daltile'>Daltile</option>
+                      <option value='Dellaterra'>Arizona Tile</option>
+                      <option value='MSI'>MSI</option>
+                      <option value='Cambria'>Cambria</option>
+                      <option value='Caesarstone'>Caesarstone</option>
+                      <option value='Viatera'>Viatera</option>
+                      <option value='Silestone'>Silestone</option>
+                      <option value='Vadara'>Vadara</option>
+                      <option value='Corianquartz'>Corian Quartz</option>
+                      <option value='Wilsonartquartz'>Wilsonart Quartz</option>
+                      <option value='Wilsonart'>Wilsonart Solid Surface</option>
+                      <option value='Corian'>Corian Solid Surface</option>
+                      <option value='Hi-macs'>LG Hi-Macs</option>
+                      <option value='Neolith'>Neolith</option>
+                      <option value='Dekton'>Dekton</option>
+                      <option value='Other'>Other</option>
+                    </select>
+                  </div>
+
+                  {/* Material Name */}
+                  <div className='md:col-span-4'>
+                    <label
+                      htmlFor='materialColor'
+                      className='block text-xs md:text-sm pl-1 font-semibold text-gray-500'
+                    >
+                      Material Color
+                    </label>
+
+                    <input
+                      type='text'
+                      id='materialColor'
+                      name='materialColor'
+                      placeholder='Material color?'
+                      className='block mt-1 w-full rounded-md border-gray-200 focus-bg-white shadow-sm sm:text-sm bg-sky-50'
+                    />
+                  </div>
+                </div>
+
+                {/* Textarea for Order Notes */}
+                <div className='grid grid-cols-1 gap-4 lg:grid-cols-6 px-4 lg:gap-x-6'>
+                  {/* Text Area for Special Notes with Customer */}
+                  <div className='col-span-4'>
+                    <label
+                      htmlFor='OrderNotes'
+                      className='block text-xs md:text-sm pl-1 font-semibold text-gray-500'
+                    >
+                      {' '}
+                      Order notes{' '}
+                    </label>
+
+                    <textarea
+                      id='OrderNotes'
+                      rows='5'
+                      className='mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm'
+                      placeholder='Enter any additional order notes...'
+                    ></textarea>
+                  </div>
+                  {/* col-span-2 */}
+                  <div className='col-span-2 my-auto'>
+                    <div className='text-center'>
+                      <button
+                        className='items-center gap-2 rounded-lg border border-blue-600 bg-blue-600 px-8 py-2 text-white hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500'
+                        type='submit'
+                      >
+                        Submit
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>

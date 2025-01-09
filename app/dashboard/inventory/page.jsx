64668e0840d126b83eb287a3ba62b97febea0b8b
Aldrin <aldrin@plamarusa.com>
2025-01-09 09:39:57 -0800

    changes in background color

--- app/dashboard/inventory/page.jsx
+++ app/dashboard/inventory/page.jsx
@@ -11,7 +11,7 @@ const InventoryPage = async () => {
     <div className='flex min-h-screen w-full flex-col'>
       <SideNavbar />
       <Header />
-      <section>
+      <section className='bg-white p-4'>
         <main className='flex flex-col sm:gap-4 sm:py-0 sm:px-0 sm:pl-14'>
           <div className='mx-auto max-w-lg text-center'>
             <h1 className='text-2xl font-bold sm:text-3xl'>Inventory Page</h1>

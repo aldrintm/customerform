67b927f1bf90b053ca7805a84d084817d626ec18
Aldrin <aldrin@plamarusa.com>
2025-01-13 19:39:12 -0800

    deleteMessage in action added

---
+++ app/actions/deleteMessage.js
@@ -0,0 +1,38 @@
+'use server'
+
+import connectDB from '@/config/db'
+import Message from '@/models/Message'
+import { getSessionUser } from '@/utils/getSession'
+import { revalidatePath } from 'next/cache'
+
+// this action is added to the form to perform tasks
+async function deleteMessage(messageId) {
+  // lets check for user session
+  const sessionUser = await getSessionUser()
+
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error('User ID is required')
+  }
+
+  // lets get the userId then
+  const { userId } = sessionUser
+
+  const message = await Message.findById(messageId)
+
+  if (message.recipient.toString() !== userId) {
+    throw new Error('Unauthorized')
+  }
+
+  await message.deleteOne()
+
+  // this will clear cached data in our form/memory
+  revalidatePath('/dashboard/message', 'page')
+
+  // redirect to newly created thank you page details
+  // redirect(`/customers/${newCustomer._id}`)
+
+  // redirect to the main table
+  // redirect(`/dashboard/message`)
+}
+
+export default deleteMessage
--- components/MessageCard.jsx
+++ components/MessageCard.jsx
@@ -2,10 +2,40 @@
 
 import { Link } from 'lucide-react'
 import { useState } from 'react'
+import { toast } from 'react-toastify'
+import markMessageAsRead from '@/app/actions/markMessageAsRead'
+import deleteMessage from '@/app/actions/deleteMessage'
 
 const MessageCard = ({ message }) => {
+  const [isRead, setIsRead] = useState(message.read)
+  const [isDeleted, setIsDeleted] = useState(false)
+
+  const handleReadClick = async () => {
+    const read = await markMessageAsRead(message._id)
+
+    setIsRead(read)
+    toast.success(`Marked as ${read ? 'Read' : 'New'}`)
+  }
+
+  const handleDeleteClick = async () => {
+    await deleteMessage(message._id)
+
+    setIsDeleted(true)
+    toast.success('Message Deleted')
+  }
+
+  if (isDeleted) {
+    return <p>Deleted Message</p>
+  }
+
   return (
     <div className='relative bg-white p-4 rounded-md shadow-md border border-gray-200'>
+      {/* Badge */}
+      {!isRead && (
+        <div className='absolute top-2 right-2 bg-yello-500 text-white px-2 py-1 rounded-md'>
+          New
+        </div>
+      )}
       <h2 className='text-xl mb-4'>
         <span className='font-bold'>Message:</span>
         {message.customer.subject}
@@ -29,10 +59,16 @@ const MessageCard = ({ message }) => {
           {new Date(message.createdAt).toLocaleString()}
         </li>
       </ul>
-      <button className='mt-4 mr-3 bg-blue-500 text-white py-1 px-3 rounded-md'>
-        Mark as Read
+      <button
+        onClick={handleReadClick}
+        className='mt-4 mr-3 bg-blue-500 text-white py-1 px-3 rounded-md'
+      >
+        {isRead ? 'Mark as New' : 'Mark as Read'}
       </button>
-      <button className='mt-4 mr-3 bg-red-500 text-white py-1 px-3 rounded-md'>
+      <button
+        onClick={handleDeleteClick}
+        className='mt-4 mr-3 bg-red-500 text-white py-1 px-3 rounded-md'
+      >
         Delete
       </button>
     </div>

e52f2e8832688f3831dcb03ed1138df2958773cf
newdles <aldrin@plamarusa.com>
2025-05-20 12:54:12 -0700

    Update TableFleet.jsx

--- components/TableFleet.jsx
+++ components/TableFleet.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useTransition } from 'react'
+import { useState, useEffect, useTransition } from 'react'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 import formatPhoneNumber from '@/app/actions/formatPhoneNumber'

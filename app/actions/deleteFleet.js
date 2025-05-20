c0f4422d4dba1494510454901e0a6a28ec0c4a30
newdles <aldrin@plamarusa.com>
2025-05-20 12:52:16 -0700

    Update deleteFleet.js

--- app/actions/deleteFleet.js
+++ app/actions/deleteFleet.js
@@ -1,4 +1,4 @@
-;('use server')
+'use server'
 
 import connectDB from '@/config/db'
 import Fleet from '@/models/Fleet'
@@ -6,44 +6,37 @@ import User from '@/models/User'
 import { getSessionUser } from '@/utils/getSession'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import mongoose from 'mongoose'
 
-//this action is deletes the customer from the database
 async function deleteVehicle(vehicleId) {
-  // lets check for user session
   const sessionUser = await getSessionUser()
 
   if (!sessionUser || !sessionUser.userId) {
     throw new Error('User ID is required')
   }
 
-  // lets get the userId then
-  const { userId } = sessionUser
+  const { userId, email } = sessionUser
 
-  // connect to DB
+  if (!mongoose.Types.ObjectId.isValid(vehicleId)) {
+    throw new Error('Invalid vehicle ID')
+  }
+
   await connectDB()
 
   const vehicle = await Fleet.findById(vehicleId)
-  const user = await User.findOne(email)
-
-  console.log(user)
+  const user = await User.findOne({ email })
 
   if (!vehicle) throw new Error('Vehicle Not Found')
 
-  // verify ownership
-  //   if (!user.isAdmin) {
-  //     throw new Error('Unauthorized')
-  //   }
+  // if (!user.isAdmin) {
+  //   throw new Error('Unauthorized')
+  // }
 
   await vehicle.deleteOne()
 
-  // this will clear cached data in our form/memory
   revalidatePath('/dashboard/company', 'page')
   revalidatePath('/dashboard/company/fleet', 'page')
 
-  // redirect to newly created thank you page details
-  // redirect(`/customers/${newCustomer._id}`)
-
-  // redirect to the main table
   redirect(`/dashboard/company`)
 }
 

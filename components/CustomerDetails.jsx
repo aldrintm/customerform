a734f118d55d5b615d603ef0cbf231fcae866e80
newdles <aldrin@plamarusa.com>
2025-06-24 11:23:34 -0700

    Update CustomerDetails.jsx

--- components/CustomerDetails.jsx
+++ components/CustomerDetails.jsx
@@ -635,7 +635,7 @@ function CustomerDetailsContent({ customer: initialCustomer, schedules }) {
                   ''
                 )}
 
-                {customer.projects[0]?.status === 'for install' ? (
+                {customer.projects[0]?.status === 'for install' || customer.projects[0]?.status === 'service' ? (
                   <div className='text-sm md:text-md font-semibold text-gray-500'>
                     <span className='text-gray-500 pr-2'>Install:</span>
                     <span className='inline-flex'>

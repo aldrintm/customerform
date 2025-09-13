39e7d51014a6bbde164240a9668f8518d6e50e31
Aldrin <aldrin@plamarusa.com>
2025-09-13 16:28:25 -0700

    Found old error using OldCustomer in in editCustomer/page.jsx

--- app/dashboard/customers/[id]/editCustomer/page.jsx
+++ app/dashboard/customers/[id]/editCustomer/page.jsx
@@ -3,7 +3,7 @@ import SideNavbar from '@/components/SideNavbar'
 
 import CustomerEditForm from '@/components/CustomerEditForm'
 import connectDB from '@/config/db'
-import Customer from '@/models/OldCustomer'
+import Customer from '@/models/Customer'
 import { convertToSerializeableObject } from '@/utils/convertToObject'
 
 const CustomerEditPage = async ({ params }) => {
